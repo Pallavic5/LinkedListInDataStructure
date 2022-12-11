b800c75ff0f74d0f934c61dc195aba878465386f
Pallavic5 <pchedge235@gmail.com>
2022-12-12 00:46:45 +0530

    Added: Logic of ability to create a Linked List by adding 30 and 56 to 70.

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.linkedlist;
 /*Problem Statement
  * UC-1 Lets create a simple Linked List of 56, 30 and 70
+ * UC-2 Ability to create Linked List by adding 30 and 56 to 70
+- Node with data 70 is First Created
+- Next 30 is added to 70
+- Finally 56 is added to 30
+- LinkedList Sequence: 56->30->70
  */
 import java.util.Scanner;
 public class LinkedList {
-	private static int data;
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		LinkedListOperations list = new LinkedListOperations();
@@ -30,8 +34,8 @@ public class LinkedList {
 				System.out.println("Select From Menu");
 			}
 		} while (choice != 3);
+		scanner.close();
 	}
-
 }
 //create Node Class
 class Node {
--- src/com/bridgelabz/linkedlist/LinkedListOperations.java
+++ src/com/bridgelabz/linkedlist/LinkedListOperations.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.linkedlist;
 /*Problem Statement
  * UC-1 Lets create a simple Linked List of 56, 30 and 70
+ *  UC-2 Ability to create Linked List by adding 30 and 56 to 70
+- Node with data 70 is First Created
+- Next 30 is added to 70
+- Finally 56 is added to 30
+- LinkedList Sequence: 56->30->70
  */
 class LinkedListOperations {
 	Node head;
