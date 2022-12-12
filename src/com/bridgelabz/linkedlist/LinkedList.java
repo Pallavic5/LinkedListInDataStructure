4fed688d27f0f1a8999a698567360630e2fdbd5c
Pallavic5 <pchedge235@gmail.com>
2022-12-12 15:21:41 +0530

    Added: Logic of ability to delete 40 from the Linked List sequence of 56->30->40->70 and show the size of LinkedList is 3.

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,19 +1,22 @@
 package com.bridgelabz.linkedlist;
-/*Problem Statement UC-8
- * Ability to insert 40 after 30 to the Linked List sequence of 56->30->70
- * - Search LinkedList to get Node with key value 30
-- Then Insert 40 to 30
-- Final Sequence: 56->30->40->70
+/*Problem Statement UC-9
+ * Ability to delete 40 from the Linked List sequence of 56->30->40->70 and show the size of LinkedList is 3
+ * Search LinkedList to find node with key value 40
+- Delete the node
+- Implement size() and show the Linked List size is 3
+- Final Sequence: 56->30->70
  */
 import java.util.Scanner;
+
 public class LinkedList {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		LinkedListOperations list = new LinkedListOperations();
 		int choice = 0;
-		//infinite loop for choice
+		// infinite loop for choice
 		do {
-			System.out.println("1.InsertFirst\n2.InsertLast\n3.InsertNthPos\n4.RemoveFirst\n5.RemoveLast\n6.SearchElement\n7.Display\n8.Exit");
+			System.out.println(
+					"1.InsertFirst\n2.InsertLast\n3.InsertNthPos\n4.RemoveFirst\n5.RemoveLast\n6.RemoveNthPosition\n7.SearchElement\n8.GetSize\n9.Display\n10.Exit");
 			System.out.println("Enter Choice:");
 			choice = scanner.nextInt();
 			switch (choice) {
@@ -36,24 +39,31 @@ public class LinkedList {
 				list.removeLast();
 				break;
 			case 6:
-				System.out.println("Enter a element for Search:");
-				list.searchNode(scanner.nextInt());
-				System.out.println();
+				System.out.println("Enter a Position.");
+				list.removeNthPosition(scanner.nextInt());
 				break;
 			case 7:
+				System.out.println("Enter a element for Search:");
+				list.searchNode(scanner.nextInt());
+				break;
+			case 8:
+				list.getSize();
+				break;
+			case 9:
 				list.display();
 				System.out.println();
 				break;
-			case 8:
+			case 10:
 				System.out.println("Byeee!!");
 				break;
 			default:
 				System.out.println("Select From Menu");
 			}
-		} while (choice != 8);
+		} while (choice != 10);
 		scanner.close();
 	}
 }
+
 //create Node Class
 class Node {
 	int data;
--- src/com/bridgelabz/linkedlist/LinkedListOperations.java
+++ src/com/bridgelabz/linkedlist/LinkedListOperations.java
@@ -1,13 +1,14 @@
 package com.bridgelabz.linkedlist;
-/*Problem Statement UC-8
- * Ability to insert 40 after 30 to the Linked List sequence of 56->30->70
- * - Search LinkedList to get Node with key value 30
-- Then Insert 40 to 30
-- Final Sequence: 56->30->40->70
+/*Problem Statement UC-9
+ * Ability to delete 40 from the Linked List sequence of 56->30->40->70 and show the size of LinkedList is 3
+ * Search LinkedList to find node with key value 40
+- Delete the node
+- Implement size() and show the Linked List size is 3
+- Final Sequence: 56->30->70
  */
 class LinkedListOperations {
 	Node head;
-	static int counter = 0;			//take counter to calculate the number of nodes present in Linkedlist
+	static int counter = 0; // take counter to calculate the number of nodes present in Linkedlist
 
 	// Method for Add Element in first position
 	public void insertFirst(int data) {
@@ -20,6 +21,7 @@ class LinkedListOperations {
 		}
 		counter++; // Increement Counter
 	}
+
 	// Create method for insert element in last position
 	public void insertLast(int data) {
 		Node newNode = new Node(data);
@@ -34,56 +36,75 @@ class LinkedListOperations {
 		}
 		counter++;
 	}
-	//Create method for insert element in Nth Position
-		public void insertNthPosition(int data, int pos) {
-			Node newNode = new Node(data);
-			if(head == null) {
-				System.out.println("List is empty!!");
+
+	// Create method for insert element in Nth Position
+	public void insertNthPosition(int data, int pos) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			System.out.println("List is empty!!");
+		} else if (pos <= 0 || pos > counter) {
+			System.out.println("Invalid Position!!");
+		} else if (pos == 1) {
+			insertFirst(data);
+		} else {
+			Node temp = head;
+			for (int i = 1; i < pos - 1; i++) {
+				temp = temp.next;
 			}
-			else if(pos <= 0 || pos > counter) {
-				System.out.println("Invalid Position!!");
-			}
-			else if(pos == 1) {
-				insertFirst(data);
-			}
-			else {
-				Node temp = head;
-				for(int i = 1; i < pos - 1 ; i++) {
-					temp = temp.next;
-				}
-				newNode.next = temp.next;
-				temp.next = newNode;
-			}
-			//counter++;
+			newNode.next = temp.next;
+			temp.next = newNode;
 		}
-		//Method for delete first element
-		public void removeFirst() {
-			if(head == null) {
-				System.out.println("List is Empty!!");
-			}
-			else {
-				Node temp = head;
-				head = head.next;
-				temp.next = null;
-				temp = null;
-			}
+		counter++;
+	}
+
+	// Method for delete first element
+	public void removeFirst() {
+		if (head == null) {
+			System.out.println("List is Empty!!");
+		} else {
+			Node temp = head;
+			head = head.next;
+			temp.next = null;
+			temp = null;
 		}
-			//Method for delete last element
-		public void removeLast() {
-			if(head == null) {
-				System.out.println("List is empty!!");
+	}
+
+	// Method for delete last element
+	public void removeLast() {
+		if (head == null) {
+			System.out.println("List is empty!!");
+		} else {
+			Node temp = head;
+			Node prev = null;
+			while (temp.next != null) {
+				prev = temp;
+				temp = temp.next;
 			}
-			else {
-				Node temp = head;
-				Node prev = null;
-				while(temp.next != null) {
-					prev = temp;
-					temp = temp.next;
-				}
-				prev.next = null;
-				temp = null;
-			}
+			prev.next = null;
+			temp = null;
 		}
+	}
+		//Method for delete element from Nth Position
+	public void removeNthPosition(int pos) {
+		if (head == null) {
+			System.out.println("List is empty!!");
+		} else if (pos <= 0 || pos > counter) {
+			System.out.println("Invalid Position!!");
+		} else if (pos == 1) {
+			removeFirst();
+		} else {
+			Node temp = head;
+			Node prev = null;
+			for (int i = 1; i < pos; i++) {
+				prev = temp;
+				temp = temp.next;
+			}
+			prev.next = temp.next;
+			temp.next = null;
+			temp = null;
+		}
+	}
+
 	// Method for Display Element
 	public void display() {
 		if (head == null) {
@@ -96,34 +117,45 @@ class LinkedListOperations {
 			}
 		}
 	}
-	 //method for search element
-	public void searchNode(int data) {  
-        Node current = head;  
-        int i = 1;  
-        boolean flag = false;  
-        //Checks whether list is empty  
-        if(head == null) {  
-            System.out.println("List is empty");  
-        }  
-        else {  
-            while(current != null) {  
-                 //Compares node to be found with each node present in the list  
-                if(current.data == data) {  
-                    flag = true;  
-                    break;  
-                }  
-                i++;  
-                current = current.next;  
-            }  
-        }  
-        if(flag)  
-             System.out.println("Element is present in the list at the position : " + i);  
-        else  
-             System.out.println("Element is not present in the list");  
-    }
-	
-  
-	
-		
 
+	// method for search element
+	public void searchNode(int data) {
+		Node current = head;
+		int i = 1;
+		boolean flag = false;
+		// Checks whether list is empty
+		if (head == null) {
+			System.out.println("List is empty");
+		} else {
+			while (current != null) {
+				// Compares node to be found with each node present in the list
+				if (current.data == data) {
+					flag = true;
+					break;
+				}
+				i++;
+				current = current.next;
+			}
+		}
+		if (flag)
+			System.out.println("Element is present in the list at the position : " + i);
+		else
+			System.out.println("Element is not present in the list");
+	}
+
+	// method for getsize of linkedlist.
+	public int getSize() {
+		int count = 0;
+		if (head == null)
+			return count;
+		else {
+			Node temp = head;
+			while (temp != null) {
+				count++;
+				temp = temp.next;
+			}
+			System.out.println("Size is " + count);
+			return count;
+		}
+	}
 }
